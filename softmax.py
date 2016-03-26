44abb3fb68214a272f7e6e01ee6735cae238c063
Dom Leone <ohheydom@gmail.com>
2016-03-26 14:14:53 -0400

    Initial commit.

---
+++ softmax.py
@@ -0,0 +1,13 @@
+class Softmax:
+    def __init__(self):
+        self.W = None
+
+    def train(self, X, y, learning_rate=0.001):
+        pass
+
+
+    def predict(self, X):
+        pass
+
+    def loss(self):
+
