9e615a7dd4798e12e7f073fedc11c6aa4a223203
Dom Leone <ohheydom@gmail.com>
2016-03-28 13:46:53 -0400

    Add more weight update methods   Implement Adam update   Implement RMSprop

--- softmax.py
+++ softmax.py
@@ -11,11 +11,15 @@ class Softmax:
         self.reg_strength = reg_strength
 
     def train(self, X, y):
-        self.W = np.random.randn(X.shape[1], y.max() + 1)
+        n_features = X.shape[1]
+        n_classes = y.max() + 1
+        self.W = np.random.randn(n_features, n_classes) / np.sqrt(n_features/2)
         config = {}
         for epoch in range(self.epochs):
             #loss = self.sgd(X, y, self.learning_rate, self.batch_size, self.reg_strength)
-            config, loss = self.sgd_with_momentum(X, y, self.learning_rate, self.batch_size, self.reg_strength, config)
+            #config, loss = self.sgd_with_momentum(X, y, self.learning_rate, self.batch_size, self.reg_strength, config)
+            config, loss = self.adam(X, y, self.learning_rate, self.batch_size, self.reg_strength, 1e-8, config)
+            #config, loss = self.rms_prop(X, y, self.learning_rate, self.batch_size, self.reg_strength, 0.99, 1e-7, config)
             print "Epoch: %s, Loss: %s" % (epoch, loss)
 
     def predict(self, X):
@@ -62,4 +66,37 @@ class Softmax:
         self.W += v
         config['velocity'] = v
         config['momentum'] = momentum
+        return config, loss
+
+    def rms_prop(self, X, y, learning_rate, batch_size, reg_strength, decay_rate, eps, cache=None):
+        random_indices = random.sample(range(X.shape[0]), batch_size)
+        X_batch = X[random_indices]
+        y_batch = y[random_indices]
+        loss, dW = self.loss(X_batch, y_batch, self.W, 0, reg_strength)
+
+        cache = np.zeros(dW.shape) if len(cache) == 0 else cache
+        cache = decay_rate * cache + (1-decay_rate) * dW**2
+        self.W -= learning_rate * dW / (np.sqrt(cache) + eps)
+        return cache, loss
+
+    def adam(self, X, y, learning_rate, batch_size, reg_strength, eps, config=None):
+        beta_1 = 0.9
+        beta_2 = 0.999
+        if config == None: config= {}
+        config.setdefault('t', 0)
+        config.setdefault('m', np.zeros(self.W.shape))
+        config.setdefault('v', np.zeros(self.W.shape))
+
+        random_indices = random.sample(range(X.shape[0]), batch_size)
+        X_batch = X[random_indices]
+        y_batch = y[random_indices]
+        loss, dW = self.loss(X_batch, y_batch, self.W, 0, reg_strength)
+
+        config['t'] += 1
+        config['m'] = beta_1 * config['m'] + (1-beta_1)*dW
+        config['v'] = beta_2 * config['v'] + (1-beta_2)*(dW**2)
+        m = config['m']/(1-beta_1**config['t'])
+        v = config['v']/(1-beta_2**config['t'])
+
+        self.W -= learning_rate*m/(np.sqrt(v) + eps)
         return config, loss
--- testing.py
+++ testing.py
@@ -8,7 +8,7 @@ y = data.target
 reg_strength = 1e-4
 batch_size = 50
 epochs = 1000
-learning_rate = 1e-2
+learning_rate = 1
 sm = Softmax(batch_size=batch_size, epochs=epochs, learning_rate=learning_rate, reg_strength=reg_strength)
 sm.train(X, y)
 pred = sm.predict(X)
